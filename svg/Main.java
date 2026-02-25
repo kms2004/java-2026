71ddc82cb7f997392aa0a0df7169b0ec38963b91
kms2004 <kamil.sienkowski777@gmail.com>
2026-02-25 16:24:09 +0100

    main, point

---
+++ svg/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    Point p1 = new Point();
+    p1.x = 5.0;
+    p1.y = 5.0;
+    System.out.println(p1.toString());
+  }
+}
+
---
+++ svg/Point.java
@@ -0,0 +1,22 @@
+import sun.security.pkcs11.P11Util;
+
+public class Point {
+  public double x;
+  public double y;
+  public String toString() {
+    return String.format("(%f,%f)",this.x,this.y);
+  }
+  public String toSvg() {
+    return String.format("<svg height=\"%f\" width=\"%f\"><circle r=\"100\" cx=\"50\" cy=\"50\" /></svg>");
+  }
+  public void translate(double dx, double dy) {
+    this.x+=dx;
+    this.y+=dy;
+  }
+  public Point translated(double dx,double dy) {
+    Point p = new Point();
+    p.x=this.x+dx;
+    p.y=this.y+dy;
+    return p;
+  }
+}
